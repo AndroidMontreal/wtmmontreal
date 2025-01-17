1bf7dfd09f3191ec58c696d6629b0fb1679352f1
Sahil Patel <smppatel999@gmail.com>
2025-01-17 18:57:09 -0500

    fix: update venue embedded map link

--- src/data/venueData.js
+++ src/data/venueData.js
@@ -4,7 +4,7 @@ export const venueData = {
     'Located near the Olympic Park, the College features unique spaces such as its Indoor Garden.',
   address: '2701 Rue Nicolet, Montréal, QC H1X 2A2',
   embededMapLink:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2793.9961928328357!2d-73.55626252339573!3d45.55040212768243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91b8b3db9383b%3A0xa42f9f309ddb9977!2s2701%20Rue%20Nicolet%2C%20Montr%C3%A9al%2C%20QC%20H1X%201Z8!5e0!3m2!1sen!2sca!4v1736637357867!5m2!1sen!2sca',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5587.924198106073!2d-73.55611392299946!3d45.55108797107569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91bf575ab9357%3A0xa64ecd7388cc64f0!2s3800%20Sherbrooke%20St%20E%2C%20Montreal%2C%20QC%20H1X%202A2!5e0!3m2!1sen!2sca!4v1737157843980!5m2!1sen!2sca',
   logo: '/images/logo/logo_college_maisonneuve.png', // Path to the venue's logo (relative to the 'public' folder)
   images: [
     '/images/venue/SalleSylvainLelievre.jpg',
