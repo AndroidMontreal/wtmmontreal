bc8f83e050fae1a8984b5d3babbeb6a79e48b12d
Eduardo Tokarski <eduardo.tokarski.almeida@gmail.com>
2025-01-21 14:36:52 -0500

    added volunteer's data

--- src/data/volunteersData.js
+++ src/data/volunteersData.js
@@ -7,5 +7,30 @@ export const volunteersData = {
       image: '/images/team/ali.jpg',
       link: 'https://devfest2023.gdgmontreal.com/speaker/ali-yazdi/',
     },
+    {
+      name: 'Anureet Kaur',
+      title: '',
+      image: '/images/team/volunteers/Anureet_Kaur.jpeg',
+      link: '',
+    },
+    {
+      name: 'Cecilia',
+      title: '',
+      image: '/images/team/volunteers/Cecilia.jpeg',
+      link: '',
+    },
+    {
+      name: 'Jacqueline',
+      title: '',
+      image: '/images/team/volunteers/Jacqueline.JPG',
+      link: '',
+    },
+    {
+      name: 'Sahil Patel',
+      title: 'Full Stack Software Developer - Cold Chain Science Enterprises',
+      image: '/images/team/volunteers/Sahil.jpg',
+      link: 'https://www.linkedin.com/in/smppatel99999/',
+    },
+    
   ],
 };
