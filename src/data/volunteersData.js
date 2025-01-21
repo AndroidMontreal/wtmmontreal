6461cc0d729748c6faab617d59e9ad13972f757b
Eduardo Tokarski <eduardo.tokarski.almeida@gmail.com>
2025-01-21 14:22:55 -0500

    added smallTitle component and volunteersData

--- src/data/volunteersData.js
+++ src/data/volunteersData.js
@@ -1,8 +1,11 @@
-export const volunteer = [
-  {
-    name: 'Ali Yazdi',
-    title: 'Senior Software Engineer at EXFO, GDG & Flutter Montreal Organizer',
-    image: '/images/team/ali.jpg',
-    link: 'https://devfest2023.gdgmontreal.com/speaker/ali-yazdi/',
-  },
-];
+export const volunteersData = {
+  title: "Volunteers",
+  volunteers: [
+    {
+      name: 'Ali Yazdi',
+      title: 'Senior Software Engineer at EXFO, GDG & Flutter Montreal Organizer',
+      image: '/images/team/ali.jpg',
+      link: 'https://devfest2023.gdgmontreal.com/speaker/ali-yazdi/',
+    },
+  ],
+};
