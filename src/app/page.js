18b751fcb8e7725ebf6a2f00844c1e77c8270a1a
Solankimimoh <solankimimoh@gmail.com>
2025-02-10 13:48:13 -0500

    fixed logo in placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-// app/page.js
+// app/dddd.js
 'use client'; // Mark this as a Client Component
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function HomePage() {
+export default function Dddd() {
   const router = useRouter();
 
   useEffect(() => {
     router.replace('/fr'); // Redirect to /fr
   }, [router]);
 
-  return null; // Render nothing while redirecting
+  return <div>{/* ... */}</div>;
 }
