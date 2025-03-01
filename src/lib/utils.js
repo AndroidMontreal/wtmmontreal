3616b5e24731a759e6f37e35479b2b2474b8dd6a
Laurence de Villers <laurence.de.villers.prog@gmail.com>
2025-03-01 18:57:41 -0500

    Order team by year and first Name (#6)
    * Order team by year and first Name
    
    * First Stefania to be the first one

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,10 +8,25 @@ export function getFirstName(fullName) {
 
 export function sortTeamByFirstName(team) {
   return team.sort((a, b) => {
-    const firstNameA = a.name.split(' ')[0].toLowerCase();
-    const firstNameB = b.name.split(' ')[0].toLowerCase();
-    if (firstNameA < firstNameB) return -1;
-    if (firstNameA > firstNameB) return 1;
-    return 0;
+    return shortByFirstName(a, b);
   });
+}
+
+export function shortByFirstName(a, b) {
+  const firstNameA = a.name.split(' ')[0].toLowerCase();
+  const firstNameB = b.name.split(' ')[0].toLowerCase();
+  if (firstNameA < firstNameB) return -1;
+  if (firstNameA > firstNameB) return 1;
+  return 0;
+}
+
+export function sortByYearAndFirstName(team) {
+  return team.sort((a,b)=>  {
+      const yearA = a.year;
+      const yearB = b.year;
+
+      if (yearA < yearB) return -1;
+      else if (yearA > yearB) return 1;
+      else return shortByFirstName(a,b);
+    });
 }
